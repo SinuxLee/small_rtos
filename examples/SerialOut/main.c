90b653922ed993ce545ba3a4c2bbe78d183302f3
sinuxlee <pingfan14@gmail.com>
2025-09-16 15:41:46 +0800

    some comments

--- examples/SerialOut/main.c
+++ examples/SerialOut/main.c
@@ -3,20 +3,22 @@
 
 void InitSerial(void)
 {
-	SCON = 0x50;
-	PCON = 0x80;
-	TMOD = TMOD & 0x0f;
+	SCON = 0x50; // 串行控制寄存器
+	PCON = 0x80; // 电源控制寄存器
+	TMOD = TMOD & 0x0f; // 定时器模式寄存器
 	TMOD = TMOD | 0x20;
 	TH1 = 0xf3;                   // com is 4800 b/s
-	TL1 = 0xf3;
-	TR1 = 1;
-	ES = 0;
-
+	TL1 = 0xf3; // TH1 + TL1 timer1 计数值
+	TR1 = 1; // timer1 控制位
+	ES = 0; // 串口中断开关
 }
 
 
 void main(void)
 {
+	InitSerial();
+	OSSemCreate(0,1);
+
     OSStart();
 }
 
@@ -33,22 +35,18 @@ void putch(uint8 Data)
         {
         } while (--j !=0);
     } while (--i != 0);
-    
 }
 
 
 void TaskA(void)
 {
-	InitSerial();
-	TMOD = (TMOD & 0XF0) | 0X01;
+    TMOD = (TMOD & 0XF0) | 0X01;
 	TL0 = 0x0;
 	TH0 = 0x0;
 	TR0 = 1;
 	ET0 = 1;
 	TF0 = 0;
     EA = 1;
-    
-	OSSemCreate(0,1);
     while (1)
     {
         OSSemPend(0,0);
